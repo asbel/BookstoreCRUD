b447763a259a74d92bf1dcd83ea7cc4ead8fc284
Asbel <aassefa1@hawk.iit.edu>
2019-10-01 15:07:05 -0500

    first commit

---
+++ JAXRSJsonCRUDExample/src/main/java/org/arpit/java2blog/bean/Order.java
@@ -0,0 +1,37 @@
+package org.arpit.java2blog.bean;
+
+public class Order{
+	
+	int id;
+	String OrderName;	
+	long population;
+	
+	public Order() {
+		super();
+	}
+	public Order(int i, String OrderName,long population) {
+		super();
+		this.id = i;
+		this.OrderName = OrderName;
+		this.population=population;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getOrderName() {
+		return OrderName;
+	}
+	public void setOrderName(String OrderName) {
+		this.OrderName = OrderName;
+	}
+	public long getPopulation() {
+		return population;
+	}
+	public void setPopulation(long population) {
+		this.population = population;
+	}	
+	
+}
